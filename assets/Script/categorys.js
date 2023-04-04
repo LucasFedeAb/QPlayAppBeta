8eb872c4e9949842f4af106899cb251ee16d5a4b
Lucas Abrigo <luca.mdq@gmail.com>
2023-04-04 00:05:55 -0300

    Cambio page categorys prueba con slider

---
+++ assets/Script/categorys.js
@@ -0,0 +1,87 @@
+// Constructor categorias
+
+class Category {
+    constructor (nombre, href, img) {
+        this.nombre = nombre
+        this.href = href
+        this.img = "../assets/Image/Categories/"+img
+    }
+}
+
+const categorys=[
+    new Category ("Geografia","./gamePlay.html","geography.jpg"),
+    new Category ("Deportes","./gamePlay.html","sports.jpg"),
+    new Category ("Historia","./gamePlay.html","history.jpg"),
+    new Category ("Programacion","./gamePlay.html","devs.jpg"),
+    new Category ("Aleatorio","./gamePlay.html","all.jpg"),
+]
+
+
+//Insertar elementos de forma dinamica
+/* let categoria = document.querySelector('#categoria');
+categorys.forEach(category=>{
+    categoria.innerHTML +=`
+    <div class="col" id="container-${category.nombre}">
+        <div class="card align-items-center ms-3">
+          <a class="text-center" href="${category.href}"><img src="${category.img}" id="img-category" class="w-50 rounded-circle" alt="${category.nombre}"></a>
+          <div class="card-body">
+            <h5 class="card-title fw-bold">${category.nombre}</h5>
+          </div>
+        </div>
+    </div>`
+}) */
+
+
+let categoria = document.querySelector('#categoria');
+categorys.forEach(category=>{
+    categoria.innerHTML +=`
+    <div class="swiper-slide">
+        <a class="text-center" href="${category.href}"> <img class="bg-image" src="${category.img}" alt="${category.nombre}"></a>
+        <img class="logo-image logo-image-1" data-swiper-parallax-x="50%" src="./images/guardians-of-the-galaxy-logo.png" alt="">
+    </div>`
+})
+
+let navCategory = document.querySelector('#liNav');
+let categorySelectNav
+
+//Ocultamos la foto del nav con css
+categorys.forEach(category=>{
+    navCategory.innerHTML +=`
+    <li><a id="${category.nombre}" class="dropdown-item nav__item__category" href="${category.href}"><img src="#" id="img-category"  alt="${category.nombre}">${category.nombre}</a></li> `
+})
+
+document.querySelectorAll('#liNav a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        // Evitar que se siga el enlace
+        /* event.preventDefault(); */
+
+        // Obtener el nombre de la categoría
+        categorySelectNav = link.querySelector('img').getAttribute('alt');
+        console.log(categorySelectNav);
+        localStorage.setItem('categorySelectNav', categorySelectNav);
+    });
+});
+
+
+
+
+// Agregar manejador de eventos click a los enlaces de las categorías
+
+let categorySelect
+
+document.querySelectorAll('#categoria a').forEach(link => {
+    link.addEventListener('click', () => {
+        // Evitar que se siga el enlace
+        /* event.preventDefault(); */
+
+        // Obtener el nombre de la categoría
+        categorySelect = link.querySelector('img').getAttribute('alt');
+        console.log(categorySelect);
+        localStorage.setItem('categorySelect', categorySelect);
+    });
+});
+
+
+
+
+
--- assets/Script/gamePlay.js
+++ assets/Script/gamePlay.js
@@ -1,11 +1,54 @@
-let categorySelect = localStorage.getItem('categorySelect');
-console.log(categorySelect);
+/* Recuperamos variables almacenadas en storage*/
+let categorySelect = localStorage.getItem('categorySelect'); 
+let categorySelectNav = localStorage.getItem('categorySelectNav');
+let SelectNavGame
 
+//variables
+let answerCategory; 
+let currentQuestion = 0; // índice de la pregunta actual
+let userAnswers = []; // array para almacenar las respuestas del usuario
+let isAnswered = false;
+let getScore = 0;
+let life = 3;
+let timer; // variable para almacenar el temporizador
 
+/* Botones nav gamePlay */
+const navSports = document.querySelector('#nav-deportes');
+const navGeography = document.querySelector('#nav-geografia');
+const navHistory = document.querySelector('#nav-historia');
+const navDevs = document.querySelector('#nav-programacion');
+const navAll = document.querySelector('#nav-aleatorio');
+
+/* Mostrar contenido */
+const showCategory = document.querySelector(".category");
+const showQuest = document.querySelector("#question");
+const showBtn0 = document.querySelector(".btn0");
+const showBtn1 = document.querySelector(".btn1");
+const showBtn2 = document.querySelector(".btn2");
+const showBtn3 = document.querySelector(".btn3");
+const btnAnswers = document.querySelector("#btn-answers");
+const timerContainer = document.querySelector(".div__time");
+const lifeContainer = document.querySelector("#life");
+
+/* Funcion asignar valor string */
+function varValue(valueString){
+    SelectNavGame = valueString;
+    localStorage.setItem('SelectNavGame', SelectNavGame);
+}
+
+/* Escuchamos click del nav */
+navSports.addEventListener("click", function() {varValue("Deportes");});
+navGeography.addEventListener("click", function() {varValue("Geografia");});
+navHistory.addEventListener("click", function() {varValue("Historia");});
+navDevs.addEventListener("click", function() {varValue("Programacion");});
+navAll.addEventListener("click", function() {varValue("Aleatorio");});
+
+/* Recupoeramos informacion de click storage */
+SelectNavGame = localStorage.getItem('SelectNavGame');
 
 /* Arrays de preguntas y respuestas */
 class Question {
-    constructor(question,answer0,answer1,answer2,answer3,correctAnswer){
+    constructor(question, answer0, answer1, answer2, answer3, correctAnswer) {
         this.question = question
         this.answer0 = answer0
         this.answer1 = answer1
@@ -16,8 +59,8 @@ class Question {
 }
 
 const geography = [
-    new Question("¿Cual es la capital de Francia?","Brujas", "Marsella", "Paris", "Bruselas",2),
-    new Question("¿Cuál es la capital de Brasil?","Brasilia", "Rio de Janeiro", "Sao Paulo", "Buenos Aires",0),
+    new Question("¿Cual es la capital de Francia?", "Brujas", "Marsella", "Paris", "Bruselas", 2),
+    new Question("¿Cuál es la capital de Brasil?", "Brasilia", "Rio de Janeiro", "Sao Paulo", "Buenos Aires", 0),
     new Question("¿Cuál es el océano más grande del mundo?", "Océano Atlántico", "Océano Pacífico", "Océano Índico", "Océano Ártico", 1),
     new Question("¿Cuál es el río más largo del mundo?", "Río Nilo", "Río Yangtze", "Río Misisipi", "Río Amazonas", 3),
     new Question("¿Cuál es el desierto más grande del mundo?", "Desierto del Sahara", "Desierto de Gobi", "Desierto de Kalahari", "Desierto de Atacama", 0),
@@ -31,8 +74,8 @@ const geography = [
 ]
 
 const sports = [
-    new Question ("¿En que deporte se destacó Mike Tyson?","Basquet ", "Boxeo", "MMA", "Ciclismo",1),
-    new Question ("¿Cuantos campeonatos del mundo tiene la Seleccion Argentina?","1", "2", "3", "4",2),
+    new Question("¿En que deporte se destacó Mike Tyson?", "Basquet ", "Boxeo", "MMA", "Ciclismo", 1),
+    new Question("¿Cuantos campeonatos del mundo tiene la Seleccion Argentina?", "1", "2", "3", "4", 2),
     new Question("¿En qué deporte se destaca Usain Bolt?", "Fútbol", "Atletismo", "Baloncesto", "Natación", 1),
     new Question("¿Quién es considerado el mejor jugador de baloncesto de todos los tiempos?", "Michael Jordan", "Kobe Bryant", "LeBron James", "Stephen Curry", 0),
     new Question("¿En qué deporte compiten los Lakers y los Celtics?", "Fútbol", "Baloncesto", "Hockey sobre hielo", "Béisbol", 1),
@@ -46,16 +89,16 @@ const sports = [
 ]
 
 const history = [
-    new Question ("¿En qué año se produjo la Revolución Francesa?","1789","1798","1812","1830",0),
-    new Question ("¿En qué año finalizó la Segunda Guerra Mundial?","1940","1950","1945","1939",2),
-    new Question("¿Qué emperador romano construyó el Coliseo?","César Augusto", "Nerón", "Trajano", "Constantino", 0),
+    new Question("¿En qué año se produjo la Revolución Francesa?", "1789", "1798", "1812", "1830", 0),
+    new Question("¿En qué año finalizó la Segunda Guerra Mundial?", "1940", "1950", "1945", "1939", 2),
+    new Question("¿Qué emperador romano construyó el Coliseo?", "César Augusto", "Nerón", "Trajano", "Constantino", 0),
     new Question("¿Qué famoso líder africano luchó por la independencia de Ghana?", "Mandela", "Haile Selassie", "Kwame Nkrumah", "Kofi Annan", 2),
     new Question("¿Qué líder militar lideró la Revolución cubana en 1959?", "Fidel Castro", "Che Guevara", "Raul Castro", "Camilo Cienfuegos", 0),
     new Question("¿Qué territorio fue anexado por Hitler en 1938, previo al inicio de la Segunda Guerra Mundial?", "Checoslovaquia", "Polonia", "Dinamarca", "Austria", 3),
     new Question("¿Quién fue el primer presidente de Estados Unidos?", "Thomas Jefferson", "George Washington", "John Adams", "Abraham Lincoln", 1),
-    new Question("¿Qué líder mundial llevó a cabo la política conocida como 'Perestroika'?","Vladimir Putin" , "Nikita Kruschev", "Boris Yeltsin","Mijail Gorbachov", 3),
+    new Question("¿Qué líder mundial llevó a cabo la política conocida como 'Perestroika'?", "Vladimir Putin", "Nikita Kruschev", "Boris Yeltsin", "Mijail Gorbachov", 3),
     new Question("¿En qué año se produjo la Guerra de los Cien Años?", "1337", "1386", "1420", "1475", 0),
-    new Question("¿Qué reina de Inglaterra fue conocida como 'La Reina Virgen'?","María I", "Victoria","Isabel I" , "Ana", 2),
+    new Question("¿Qué reina de Inglaterra fue conocida como 'La Reina Virgen'?", "María I", "Victoria", "Isabel I", "Ana", 2),
     new Question("¿Qué rey de España financió el viaje de Cristóbal Colón al Nuevo Mundo?", "Fernando II de Aragón", "Carlos I de España", "Felipe II de España", "Isabel I de Castilla", 3),
     new Question("¿Qué líder mundial conocido como el 'Padre de la Nación' luchó por la independencia de la India?", "Nelson Mandela", "Mahatma Gandhi", "Jawaharlal Nehru", "Indira Gandhi", 0)
 ]
@@ -63,68 +106,93 @@ const history = [
 const devs = [
     new Question("¿Qué significa HTML?", "Hypertext Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "Hyperspace Text Markup Language", 0),
     new Question("¿Qué propiedad de CSS se utiliza para cambiar el color de fondo de un elemento?", "color", "background-color", "border-color", "font-color", 1),
-    new Question("¿Qué método de JavaScript se utiliza para agregar un nuevo elemento al final de un array?","unshift()" , "pop()", "shift()", "push()", 3),
+    new Question("¿Qué método de JavaScript se utiliza para agregar un nuevo elemento al final de un array?", "unshift()", "pop()", "shift()", "push()", 3),
     new Question("¿Qué elemento de HTML se utiliza para incluir CSS en una página?", "<style>", "<script>", "<link>", "<head>", 2),
     new Question("¿Qué tipo de valor devuelve el método parseInt() de JavaScript?", "booleano", "número entero", "cadena de texto", "flotante", 1),
     new Question("¿Qué propiedad de CSS se utiliza para cambiar la posición de un elemento?", "position", "top", "left", "margin", 0),
     new Question("¿Qué evento de JavaScript se utiliza para detectar cuando un usuario hace clic en un elemento?", "onchange", "onclick", "onmouseclick", "onmouseover", 1),
     new Question("¿Qué elemento de HTML se utiliza para definir un enlace?", "<link>", "<a>", "<href>", "<url>", 1),
     new Question("¿Qué método de JavaScript se utiliza para agregar un nuevo elemento al inicio de un array?", "push()", "pop()", "shift()", "unshift()", 3),
-    new Question("¿Qué propiedad de CSS se utiliza para cambiar el tamaño de fuente de un elemento?", "text-size", "size","font-size", "text-font", 2)
+    new Question("¿Qué propiedad de CSS se utiliza para cambiar el tamaño de fuente de un elemento?", "text-size", "size", "font-size", "text-font", 2)
 ]
 
 //Funcion agregar categoria con for of
-const addCategory = (array,nameCategory) => {
+const addCategory = (array, nameCategory) => {
     for (const elemento of array) {
         elemento.category = nameCategory;
     }
 }
-addCategory(geography,"GEOGRAFIA");
-addCategory(sports,"DEPORTES");
-addCategory(history,"HISTORIA");
-addCategory(devs,"PROGRAMACION");
+addCategory(geography, "GEOGRAFIA");
+addCategory(sports, "DEPORTES");
+addCategory(history, "HISTORIA");
+addCategory(devs, "PROGRAMACION");
 //Creamos array todas las categorias
-export const allCategory = geography.concat(sports,history,devs);
+export const allCategory = geography.concat(sports, history, devs);
 
-let answerCategory
-switch (categorySelect) {
-    case "Geografia":
-        answerCategory = geography;
-        
-        break;
-    case "Deportes":
-        answerCategory = sports;
-        break;
-    case "Historia":
-        answerCategory = history;
-        break;
-    case "Programacion":
-        answerCategory = devs;
-        break;
-    case "Aleatorio":
-        answerCategory = allCategory;
-    break;
-  
+/* Funcion para acceder a array segun click categorias  */
+function clickCategory(btnCategory) {
+    switch (btnCategory) {
+        case "Geografia":
+            answerCategory = geography;
+    
+            break;
+        case "Deportes":
+            answerCategory = sports;
+            break;
+        case "Historia":
+            answerCategory = history;
+            break;
+        case "Programacion":
+            answerCategory = devs;
+            break;
+        case "Aleatorio":
+            answerCategory = allCategory;
+            break;
+    
+    }
+    
 }
 
-
+/* Verificar categoria seleccionada segun nav o galeria */
+if (categorySelect) {
+    clickCategory(categorySelect);
+    localStorage.removeItem('categorySelect')
+    localStorage.removeItem('categorySelectNav')
+    localStorage.removeItem('SelectNavGame')
+    
+} else if (categorySelectNav) {
+    clickCategory(categorySelectNav);
+    localStorage.removeItem('categorySelect')
+    localStorage.removeItem('categorySelectNav')
+    localStorage.removeItem('SelectNavGame')
+}else if (SelectNavGame) {
+    clickCategory(SelectNavGame);
+    localStorage.removeItem('categorySelect')
+    localStorage.removeItem('categorySelectNav')
+    localStorage.removeItem('SelectNavGame')
+}
+console.log(answerCategory);
 
 // función para barajar el array usando el método Fisher-Yates shuffle
 function shuffleArray(array) {
+
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
 }
-  
-// barajar el array quizData
-shuffleArray(answerCategory);
 
-let currentQuestion = 0; // índice de la pregunta actual
-let userAnswers = []; // array para almacenar las respuestas del usuario
-let isAnswered = false; // indica si ya se ha respondido a la pregunta actual
-let getScore = 0;
+
+if (answerCategory === undefined) {
+    answerCategory = sports; //Categoria por defecto si actualiza el navegador
+    // barajar el array almacenado en answerCategory
+    shuffleArray(answerCategory);
+}else {
+    shuffleArray(answerCategory);
+}
+
+
 
 // función para verificar la respuesta del usuario
 function checkAnswer(selectedAnswer) {
@@ -132,7 +200,7 @@ function checkAnswer(selectedAnswer) {
     if (isAnswered) {
         return;
     }
-    
+
     // marcar que ya se ha respondido a la pregunta actual
     isAnswered = true;
 
@@ -143,28 +211,31 @@ function checkAnswer(selectedAnswer) {
     // verificar si la respuesta es correcta y actualizar el botón
     let btn = document.querySelector(".btn" + selectedAnswer);
     let textAlert = document.querySelector(".alert__msg");
-    
+
     if (selectedAnswer === answerCategory[currentQuestion].correctAnswer) {
 
         btn.classList.add("correct");
         textAlert.classList.add("textCorrect");
-        getScore= getScore+10;
+        getScore = getScore + 10;
         document.querySelector(".alert__msg").innerText = `Correcto`;
         document.querySelector(".score").innerText = `Score: ${getScore}`;
-        
-    } else {    
+
+    } else {
+        life--;
+        console.log(life);
         btn.classList.add("incorrect");
         textAlert.classList.add("textIncorrect");
-        document.querySelector(".alert__msg").innerText = `Incorrecto`;
+        textAlert.innerText = `Incorrecto`;
         // resaltar la respuesta correcta
         document.querySelector(`.btn${answerCategory[currentQuestion].correctAnswer}`).classList.add("correct");
-        
+
     }
-    
+
+
     // guardar la respuesta del usuario
     userAnswers[currentQuestion] = selectedAnswer;
 
-   
+
     // pasar a la siguiente pregunta después de 1 segundo
     setTimeout(function () {
         // restablecer los botones
@@ -175,10 +246,10 @@ function checkAnswer(selectedAnswer) {
         document.querySelector(".alert__msg").innerText = ` `;
         document.querySelector(`.btn${answerCategory[currentQuestion].correctAnswer}`).classList.remove("correct");
         // pasar a la siguiente pregunta o mostrar el puntaje final
-        console.log(currentQuestion);
-        if (currentQuestion < answerCategory.length -1) {
+        //console.log(currentQuestion);
+        if (currentQuestion < answerCategory.length - 1 && life > 0) {
             currentQuestion++
-          
+
             showQuestion();
         } else {
             showScore();
@@ -189,38 +260,37 @@ function checkAnswer(selectedAnswer) {
 }
 
 // función para mostrar la pregunta y las respuestas posibles
-const showCategory = document.querySelector('#categorias');
+
 function showQuestion() {
 
     // restablecer el temporizador
     clearInterval(timer);
-    document.getElementById("timer").innerText = 10;
+    document.querySelector("#timer").innerText = 10;
+    document.querySelector("#life").innerText = life;
 
     // mostrar la pregunta y las respuestas posibles
-    document.querySelector(".category").innerText = answerCategory[currentQuestion].category;
-    document.getElementById("question").innerText = answerCategory[currentQuestion].question;
-    document.querySelector(".btn0").innerText = answerCategory[currentQuestion].answer0;
-    document.querySelector(".btn1").innerText = answerCategory[currentQuestion].answer1;
-    document.querySelector(".btn2").innerText = answerCategory[currentQuestion].answer2;
-    document.querySelector(".btn3").innerText = answerCategory[currentQuestion].answer3;
+    showCategory.innerText = answerCategory[currentQuestion].category;
+    showQuest.innerText = answerCategory[currentQuestion].question;
+    showBtn0.innerText = answerCategory[currentQuestion].answer0;
+    showBtn1.innerText = answerCategory[currentQuestion].answer1;
+    showBtn2.innerText = answerCategory[currentQuestion].answer2;
+    showBtn3.innerText = answerCategory[currentQuestion].answer3;
 
     // activar los botones de respuesta
-  
-    document.querySelector(".btn0").disabled = false;
-    document.querySelector(".btn0").addEventListener("click", function() { checkAnswer(0); });
-    document.querySelector(".btn1").disabled = false;
-    document.querySelector(".btn1").addEventListener("click", function() { checkAnswer(1); });
-    document.querySelector(".btn2").disabled = false;
-    document.querySelector(".btn2").addEventListener("click", function() { checkAnswer(2); });
-    document.querySelector(".btn3").disabled = false;
-    document.querySelector(".btn3").addEventListener("click", function() { checkAnswer(3); });
+
+    showBtn0.disabled = false;
+    showBtn0.addEventListener("click", function () { checkAnswer(0); });
+    showBtn1.disabled = false;
+    showBtn1.addEventListener("click", function () { checkAnswer(1); });
+    showBtn2.disabled = false;
+    showBtn2.addEventListener("click", function () { checkAnswer(2); });
+    showBtn3.disabled = false;
+    showBtn3.addEventListener("click", function () { checkAnswer(3); });
 
 
     // iniciar el temporizador
     startTimer();
 }
-
-let timer; // variable para almacenar el temporizador
 
 
 // establecer el tiempo inicial
@@ -232,16 +302,18 @@ function startTimer() {
         // restablecer el temporizador si se acaba el tiempo
         if (time <= 0) {
             clearInterval(timer);
+            life--;
             // pasar a la siguiente pregunta
             setTimeout(function () {
                 // pasar a la siguiente pregunta o mostrar el puntaje final
-                if (currentQuestion < answerCategory.length - 1) {
+                if (currentQuestion < answerCategory.length - 1 && life > 0) {
                     currentQuestion++;
-                
+
                     showQuestion();
                 } else {
                     showScore();
                 }
+
             }, 1000);
 
         }
@@ -254,31 +326,30 @@ function startTimer() {
 
 // función para mostrar el puntaje final
 
-const btnAnswers = document.querySelector("#btn__answers");
-const timerContainer = document.querySelector("#timer-container");
-
 function showScore() {
     // desactivar los botones de respuesta
-    document.querySelector(".btn0").disabled = true;
-    document.querySelector(".btn1").disabled = true;
-    document.querySelector(".btn2").disabled = true;
-    document.querySelector(".btn3").disabled = true;
-    btnAnswers.remove("#btn__answers");
-    timerContainer.remove("#timer-container");
+    showBtn0.disabled = true;
+    showBtn1.disabled = true;
+    showBtn2.disabled = true;
+    showBtn3.disabled = true;
+    btnAnswers.remove("#btn-answers");
+    timerContainer.remove(".div__time");
+    lifeContainer.innerText = life;
+    
 
-  
     // mostrar el puntaje final
     let score = 0;
     for (let i = 0; i < answerCategory.length; i++) {
-      if (userAnswers[i] === answerCategory[i].correctAnswer) {
-        score= score+10;
-      }
+        if (userAnswers[i] === answerCategory[i].correctAnswer) {
+            score = score + 10;
+        }
     }
-    let scoreText = `Score FINAL: ${score}`;
+    let scoreText = `SCORE FINAL: ${score}`;
     let scoreFin = document.querySelector(".score");
-    scoreFin.classList.add("scoreFin");
     scoreFin.classList.remove(".score");
-    document.querySelector(".scoreFin").innerText = scoreText;
+    scoreFin.classList.add("scoreFin");
+    scoreFin.innerText = scoreText;
+
 }
 // iniciar la prueba al cargar la página
 showQuestion();
